4f7dd0b71e42387ae91f79cb21b6b9cde56e001e
michael <maydev99@gmail.com>
2020-06-22 18:32:49 -0400

    Finished AnotherCustom Button

--- app/src/main/java/com/bombadu/custombutton/AnotherCustomView.kt
+++ app/src/main/java/com/bombadu/custombutton/AnotherCustomView.kt
@@ -7,7 +7,7 @@ import android.graphics.Paint
 import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
-import kotlin.math.min
+import androidx.core.content.withStyledAttributes
 
 private enum class ButtonState(val label: Int) {
     OFF(R.string.button_off),
@@ -24,39 +24,62 @@ class AnotherCustomView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    private var textSize = 0.0f
+    private var buttonOffColor = 0
+    private var buttonOnColor = 0
+    private var buttonState = ButtonState.OFF
 
-    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
-        textSize = 35.0f
-        color = Color.RED
         typeface = Typeface.create("", Typeface.BOLD)
+
+
+
     }
 
-    private val buttonOnPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        style = Paint.Style.FILL
-        color = Color.BLUE
-    }
-
-    private val buttonOffPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        style = Paint.Style.FILL
-        color = Color.YELLOW
-    }
 
     init {
         isClickable = true
 
+        context.withStyledAttributes(attrs, R.styleable.CoolButtonView) {
+            buttonOffColor = getColor(R.styleable.CoolButtonView_buttonColor1, 0)
+            buttonOnColor = getColor(R.styleable.CoolButtonView_buttonColor2, 0)
+
+        }
+
     }
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        textSize = (min(w,h) / 3.0).toFloat()
+    override fun performClick(): Boolean {
+        if (super.performClick()) return true
+        buttonState = buttonState.next()
+        contentDescription = resources.getString(buttonState.label)
+        invalidate()
+
+        return true
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        canvas!!.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), (width / 2).toFloat(), buttonOffPaint)
+        paint.color = when(buttonState) {
+
+            ButtonState.OFF -> buttonOffColor
+            ButtonState.ON -> buttonOnColor
+        }
+
+
+        //paint.setShadowLayer((width /2).toFloat(),(height / 2).toFloat(),12.0f,Color.DKGRAY)
+        canvas!!.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), (width / 2).toFloat(), paint)
+
+        for (i in ButtonState.values()) {
+            val label = resources.getString(buttonState.label)
+            paint.color = Color.WHITE
+            paint.textSize = (width / 3).toFloat()
+            canvas.drawText(label, (width / 2).toFloat(), (height / 1.6).toFloat(), paint)
+        }
+
     }
 
 
